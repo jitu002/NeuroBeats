f9702eaf0d302222f90a6ba0396f781a71f83677
Ajit Kumar Mishra <ajitmishra1402@gmail.com>
2024-08-02 12:33:15 +0530

    fav music feature

--- app/src/main/java/com/neurobeat/neurobeats/pages/view/Library.kt
+++ app/src/main/java/com/neurobeat/neurobeats/pages/view/Library.kt
@@ -1,10 +1,34 @@
 package com.neurobeat.neurobeats.pages.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.LibraryMusic
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -14,8 +38,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.google.firebase.auth.FirebaseAuth
@@ -25,10 +54,17 @@ import com.neurobeat.neurobeats.api.models.Track
 import com.neurobeat.neurobeats.music.view.TrackItemView
 import com.neurobeat.neurobeats.pages.viewmodel.LibraryViewModel
 import com.neurobeat.neurobeats.ui.theme.BackgroundColor
+import com.neurobeat.neurobeats.ui.theme.BarColor
+import com.neurobeat.neurobeats.ui.theme.txtColor
+import kotlinx.coroutines.launch
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Library(navController: NavController, accessToken: String){
+
+    val topScrollBehavior= TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+    val bottomScrollBehavior= BottomAppBarDefaults.exitAlwaysScrollBehavior()
 
     val dbOps=DatabaseOperation()
     val coroutineScope= rememberCoroutineScope()
@@ -63,23 +99,72 @@ fun Library(navController: NavController, accessToken: String){
             println("Library screen fav:$favTracks")
         }
     }
-
-    LazyColumn(
+    Scaffold(
         modifier = Modifier
-            .fillMaxSize()
-            .background(BackgroundColor),
-        contentPadding = PaddingValues(16.dp)
-    ) {
-        //ui GOES here
+            .nestedScroll(topScrollBehavior.nestedScrollConnection)
+            .nestedScroll(bottomScrollBehavior.nestedScrollConnection),
 
-        items(favTracks) { trackItem ->
-            TrackItemView(track = trackItem) {
-                val artistsIds = trackItem.artists.joinToString(",") { it.id }
-                val trackId = trackItem.id
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = BarColor,
+                    titleContentColor = txtColor,
+                ),
+                title = {
+                    Text(
+                        "Library",
+                        maxLines = 3,
+                        fontSize =27.sp,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
 
-                navController.navigate("MusicPlayer/$accessToken/$trackId/$albumId/$artistsIds/$fromArtist")
+                scrollBehavior = topScrollBehavior,
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                containerColor = BarColor,
+                contentColor = txtColor,
+                scrollBehavior = bottomScrollBehavior
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    IconButton(onClick = { navController.navigate("Homepage") }) {
+                        Icon(imageVector = Icons.Default.Home, contentDescription ="Home icon" )
+                    }
+                    IconButton(onClick = { navController.navigate("Search") }) {
+                        Icon(imageVector = Icons.Default.Search, contentDescription = "Search icon")
+                    }
+                    IconButton(onClick = { navController.navigate("Library/$accessToken") },colors= IconButtonDefaults.iconButtonColors(Color.Magenta)){
+                        Icon(imageVector = Icons.Default.LibraryMusic, contentDescription = "Library music")
+                    }
+                }
             }
         }
+    ) { innerPadding ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .background(BackgroundColor),
+            contentPadding = PaddingValues(16.dp)
+        ) {
+            //ui GOES here
 
+            items(favTracks) { trackItem ->
+                TrackItemView(track = trackItem) {
+                    val artistsIds = trackItem.artists.joinToString(",") { it.id }
+                    val trackId = trackItem.id
+
+                    navController.navigate("MusicPlayer/$accessToken/$trackId/$albumId/$artistsIds/$fromArtist")
+                }
+            }
+
+        }
     }
+
 }
